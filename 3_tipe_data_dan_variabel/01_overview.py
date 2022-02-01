f2fca198c233767e8a1431c27534ef14dfe3b86d
Delicousz <66639433+Delicousz@users.noreply.github.com>
2022-02-01 23:32:15 +0700

    Delete 3_tipe_data_dan_variabel directory

--- 3_tipe_data_dan_variabel/01_overview.py
+++
@@ -1,7 +0,0 @@
-nama = 'Nurul Huda'
-usia = 24
-sudah_menikah = True
-
-print('nama =', nama)
-print('usia =', usia)
-print('sudah_menikah =', sudah_menikah)
--- 3_tipe_data_dan_variabel/04_beda_numerik_dan_string.py
+++
@@ -1,5 +0,0 @@
-# penjumlahan dua data numerik
-print(5 + 5) # output 10
-
-# penjumlahan dua data string
-print('5' + '5') # output 55

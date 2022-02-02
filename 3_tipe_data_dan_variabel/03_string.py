35601d2e1d81d09ecf704ee7436f38b8c4e8820d
Delicousz <66639433+Delicousz@users.noreply.github.com>
2022-02-02 09:59:43 +0700

    Add files via upload

---
+++ 3_tipe_data_dan_variabel/03_string.py
@@ -0,0 +1,13 @@
+nama_depan = "Robby"
+nama_belakang = 'Abdullah'
+nama_lengkap = nama_depan + ' ' + nama_belakang
+usia = '19'
+alamat = 'Depok'
+kata_mutiara = "Don't judge a book by it's cover"
+
+print(nama_lengkap, '(' + usia + ')', ',', 'dari', alamat, ', kata mutiara:', kata_mutiara)
+
+print('\nTipe dari nama_lengkap:', type(nama_lengkap))
+print('Tipe dari usia:', type(usia))
+print('Tipe dari alamat:', type(alamat))
+print('Tipe dari kata_mutiara:', type(kata_mutiara))
---
+++ 3_tipe_data_dan_variabel/string.py
@@ -0,0 +1,11 @@
+nama_depan = 'Berlin'
+nama_belakang = "Nia"
+
+nama_lengkap = nama_depan + ' ' + nama_belakang
+
+alamat = """Jalan Alternatif UI 100
+Kecamatan Cimanggis
+Kota Surabaya
+Provinsi Jawa Timur"""
+
+usia = 10

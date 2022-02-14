e1b60e46ef93498c6fdc2e2976a402b90558667c
Delicousz <66639433+Delicousz@users.noreply.github.com>
2022-02-15 01:51:13 +0700

    Add files via upload

---
+++ 6_operator/02_perbandingan.py
@@ -0,0 +1,8 @@
+a, b = 13, 14
+
+print(a, '>', b, '=', a > b) # lebih dari
+print(a, '<', b, '=', a < b) # kurang dari 
+print(a, '==', b, '=', a == b) # sama dengan 
+print(a, '!=', b, '=', a != b) # tidak sama dengan 
+print(a, '>=', b, '=', a >= b) # lebih dari sama dengan
+print(a, '<=', b, '=', a <= b) # kurang dari sama dengan 
---
+++ 6_operator/05_keanggotaan.py
@@ -0,0 +1,19 @@
+perusahaan = 'Microsoft'
+list_pulau = ['Jawa', 'Sumatra', 'Sulawesi']
+
+# ini adalah dictionary, insyaallah akan kita pelajari
+# di pertemuan-pertemuan yang akan datang
+mahasiswa = {
+  'nama': 'merlin nur',
+  'asal': 'osaka'
+}
+
+print(perusahaan)
+print(list_pulau)
+print(mahasiswa)
+print("Apakah 'c' ada di variabel perusahaan?", 'c' in perusahaan)
+print("Apakah 'z' tidak ada di variabel perusahaan?", 'z' not in perusahaan)
+print("Apakah 'Madura' ada di variabel list_pulau?", 'Madura' in perusahaan)
+print("Apakah 'Madura' tidak ada di variabel list_pulau?", 'Madura' not in perusahaan)
+print("Apakah atribut 'nama' ada di variabel mahasiswa?", 'nama' in mahasiswa)
+

6f341258cc2c7f3cdf569bc887bfb03bf1f36897
Delicousz <66639433+Delicousz@users.noreply.github.com>
2022-02-15 02:25:55 +0700

    Add files via upload

---
+++ 8_output/01_tanpa_argumen.py
@@ -0,0 +1,10 @@
+print('Selamat pagi Andi')
+
+
+print('Selamat pagi', 'Bocah Kemo!')
+
+
+print('Selamat', 'pagi', 'baby!')
+
+
+print('Tinkywinky', 'Dipsy', 'Lala', 'Po')
---
+++ 8_output/02_separator.py
@@ -0,0 +1,11 @@
+print('Tinkywinky', 'Dipsy', 'Lala', 'Po', sep='^_^')
+# Tinkywinky^_^Dipsy^_^Lala^_^Po
+
+print('Fatih', 'Wawa', 'Farid', 'Faris', sep='\n\n')
+# Fatih
+# 
+# Wawa
+# 
+# Farid
+# 
+# Faris

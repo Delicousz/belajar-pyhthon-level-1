6b151a7dbeb43cdcd0515ef2bd11bdc492be36ed
Delicousz <66639433+Delicousz@users.noreply.github.com>
2022-02-19 17:18:59 +0700

    Delete 7_input directory

--- 7_input/01_string.py
+++
@@ -1,4 +0,0 @@
-nama = input('Masukkan nama anda: ')
-
-print('Halo,', nama, '.. selamat datang!\n')
-print('Variabel nama bertipe data:', type(nama),'\n')
--- 7_input/02_int.py
+++
@@ -1,7 +0,0 @@
-print('Kalkulator luas persegi panjang\n')
-panjang = input('Masukkan panjang: ')
-lebar = input('Masukkan lebar: ')
-
-print('Luas =', int(panjang) * int(lebar),'\n')
-print('variabel panjang bertipe data = ', type(panjang))
-print('variabel lebar bertipe data = ', type(lebar),'\n')

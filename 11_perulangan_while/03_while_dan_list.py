ef4b935fca331e2e9bcdf2884fb118d1b4673381
Delicousz <66639433+Delicousz@users.noreply.github.com>
2022-02-19 19:44:02 +0700

    Add files via upload

---
+++ 11_perulangan_while/03_while_dan_list.py
@@ -0,0 +1,14 @@
+listnama = [
+  'Fauzi', 'Ukay', 'Dewi', 'Berlin'
+]
+# bermain index
+i = 0
+while i < len(listnama):
+  print(listnama[i])
+  i += 1
+
+print('----------------')
+
+# bermain pop
+while listnama:
+  print(listnama.pop(0))
---
+++ 11_perulangan_while/04_while_dengan_inputan.py
@@ -0,0 +1,6 @@
+a = int(input('Masukkan bilangan ganjil lebih dari 50: '))
+
+while a % 2 != 1 or a <= 31:
+  a = int(input('Salah, masukkan lagi: '))
+
+print('Benar')

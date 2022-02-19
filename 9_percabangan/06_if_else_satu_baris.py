d20e3435c10b5d4bbfb4f3706bede9b789142ac8
Delicousz <66639433+Delicousz@users.noreply.github.com>
2022-02-19 16:33:34 +0700

    Delete 9_percabangan directory

--- 9_percabangan/06_if_else_satu_baris.py
+++
@@ -1,9 +0,0 @@
-nilai = int(input('Masukkan nilai: '))
-status = 'lulus' if nilai >= 70 else 'tidak lulus'
-
-# if nilai >= 70:
-#   status = 'lulus'
-# else:
-#   status = 'tidak lulus'
-
-print(status)

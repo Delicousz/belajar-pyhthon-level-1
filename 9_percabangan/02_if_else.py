45a8bcb479d8bd9a29b74595424883c023642ff0
Delicousz <66639433+Delicousz@users.noreply.github.com>
2022-02-19 16:34:40 +0700

    Add files via upload

---
+++ 9_percabangan/02_if_else.py
@@ -0,0 +1,9 @@
+nilai = int(input('Masukkan nilai anda: '))
+
+print('Nilai anda adalah:', nilai, '\n')
+
+if nilai >= 70:
+  print('Selamat, anda lulus! 😮‍💨🤗')
+else:
+  print('Maaf, anda tidak lulus 🤪')
+
